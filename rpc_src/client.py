92bc3ff4e827e8cc08bced716cb9d7366f6209c7
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2021-05-22 17:59:35 +0300

    Stage 2.2, RPCing C++ DLL functions, done and checked locally.

--- rpc_src/client.py
+++ rpc_src/client.py
@@ -1,15 +1,46 @@
 #!/usr/bin/env python
 
 import pika
+import sys
+import os
+import uuid
+corr_id = str(uuid.uuid4())
+response = ''
+def on_response(ch, method, props, body):
+    if corr_id == props.correlation_id:
+        globals()['response'] = str(body, "utf-8")
+        print(f"!!! GOT RESPONSE: {response}")
 
+
+# establishing connection
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-channel.queue_declare('rpcdemo')
+result = channel.queue_declare('', exclusive=True)
+callback_queue = result.method.queue
+
+channel.basic_consume(auto_ack=True,
+                      queue=callback_queue,
+                      on_message_callback=on_response)
+
+# preparing request from cmdline arguments
+if(len(sys.argv) < 4):
+    print("NOPE")
+    os._exit(1)
+num1 = sys.argv[1]
+op = sys.argv[2]
+num2 = sys.argv[3]
+request = " ".join([num1, op, num2])
 
 channel.basic_publish(exchange='',
                       routing_key='rpcdemo',
-                      body='OMG DOES IT ACTUALLY WROK?!')
-print("!!! Sent a message to the 'rpcdemo' queue!")
+                      body=request,
+                      properties=pika.BasicProperties(
+                          reply_to=callback_queue,
+                          correlation_id=corr_id
+                      ))
+print(f"!!! Sent request for {request}, waiting for response!")
+while response == '':
+    connection.process_data_events()
 
 connection.close()
--- rpc_src/server.py
+++ rpc_src/server.py
@@ -18,19 +18,52 @@ def mul_two_cpp(one, two):
     cpp_so.mul_ints.restype = ctypes.c_ulonglong
     return cpp_so.mul_ints(one, two)
 
-def main():
-    def callback(ch, method, properties, body):
-        print("!!! RECEIVED: [{0}]".format(str(body, "utf-8")))
+def prepare_request_from_bytes(bodybytes):
+    bodystr = str(bodybytes, "utf-8")
+    bodylist = list(filter(None, bodystr.split(" ")))
+    return bodylist
 
+def on_request(ch, method, props, body):
+    print("!!! GOT REQUEST: {0}".format(str(body, "utf-8")))
+    # translating request for server to understand
+    request = prepare_request_from_bytes(body)
+
+    # calculating response
+    # error handling
+    response = ''
+    if (len(request) != 3 or (request[1] != '*' and request[1] != '+')):
+        response = 'NOPE'
+    try:
+        request[0] = int(request[0])
+        request[2] = int(request[2])
+    except ValueError:
+        response = 'NOPE'
+    # actual response generation
+    if response != 'NOPE':
+        if request[1] == '*':
+            response = str(mul_two_cpp(request[0], request[2]))
+        elif request[1] == '+':
+            response = str(add_two_cpp(request[0], request[2]))
+
+    print(f"!!! RESPONSE IS {response}, sending...")
+    # sending the response back to client
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id=props.correlation_id),
+                     body=response)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = connection.channel()
 
     channel.queue_declare('rpcdemo')
 
-    channel.basic_consume(auto_ack=True,
-                        queue='rpcdemo',
-                        on_message_callback=callback)
-    print("Waiting to receive messages from the 'rpcdemo' queue... Press [Ctrl + C] to exit.")
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='rpcdemo',
+                          on_message_callback=on_request)
+    print("Waiting for new RPC requests... Press [Ctrl + C] to exit.")
     channel.start_consuming()
 
 if __name__ == "__main__":
